84f64e632ab29fd6b7d2b41f9d2537c4464e6911
Nathan Scott-Smith <nathanljss@hotmail.com>
2024-07-03 14:31:58 +0100

    Added some notes for a laugh

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+console.log("Just a cheeky website innit for practising git");
+// git add .
+// git commit -m "Wack a message in here if you want"
+// git push
+// ???
+// profit
