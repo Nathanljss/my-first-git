39d2fcddcef6a079aa768e770a3c8d6f25c38e50
Nathan Scott-Smith <nathanljss@hotmail.com>
2024-07-03 14:39:28 +0100

    Maybe fixed now?

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Hello world");
 console.log("Just a cheeky website innit for practising git");
 console.log("git add .");
-console.log("git commit -m "Wack a message in here if you want"");
+console.log("git commit -m Wack a message in here if you want");
 console.log("git push");
 console.log("???");
 console.log("profit");
